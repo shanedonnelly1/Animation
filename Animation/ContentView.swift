36e2b4c14ebcd2833ed8b23da0c0b80bcf0434ee
Shane Donnelly <shanedonnelly1@hotmail.com>
2020-08-17 12:39:50 +0100

    Initial Commit

---
+++ Animation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation
+//
+//  Created by Shane Donnelly on 17/08/2020.
+//  Copyright © 2020 Shane Donnelly. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
